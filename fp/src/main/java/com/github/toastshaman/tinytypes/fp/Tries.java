fd9b2324deeeeaff961749ff9599df22d3a36cd2
Kevin Denver <1027637+ToastShaman@users.noreply.github.com>
2023-11-26 09:10:16 +0000

    fix: make Try<T> helper methods public

--- fp/src/main/java/com/github/toastshaman/tinytypes/fp/Tries.java
+++ fp/src/main/java/com/github/toastshaman/tinytypes/fp/Tries.java
@@ -24,20 +24,20 @@ public final class Tries {
         return t1.flatMap(a -> t2.flatMap(b -> t3.flatMap(c -> t4.map(d -> fn.apply(a, b, c, d)))));
     }
 
-    static <T1, T2, U> Try<U> flatZip(Try<T1> t1, Try<T2> t2, Function2<T1, T2, Try<U>> fn) {
+    public static <T1, T2, U> Try<U> flatZip(Try<T1> t1, Try<T2> t2, Function2<T1, T2, Try<U>> fn) {
         return t1.flatMap(a -> t2.flatMap(b -> fn.apply(a, b)));
     }
 
-    static <T1, T2, T3, U> Try<U> flatZip(Try<T1> t1, Try<T2> t2, Try<T3> t3, Function3<T1, T2, T3, Try<U>> fn) {
+    public static <T1, T2, T3, U> Try<U> flatZip(Try<T1> t1, Try<T2> t2, Try<T3> t3, Function3<T1, T2, T3, Try<U>> fn) {
         return t1.flatMap(a -> t2.flatMap(b -> t3.flatMap(c -> fn.apply(a, b, c))));
     }
 
-    static <T1, T2, T3, T4, U> Try<U> flatZip(
+    public static <T1, T2, T3, T4, U> Try<U> flatZip(
             Try<T1> t1, Try<T2> t2, Try<T3> t3, Try<T4> t4, Function4<T1, T2, T3, T4, Try<U>> fn) {
         return t1.flatMap(a -> t2.flatMap(b -> t3.flatMap(c -> t4.flatMap(d -> fn.apply(a, b, c, d)))));
     }
 
-    static <T> Tuple2<List<T>, List<Throwable>> partition(Iterable<Try<T>> values) {
+    public static <T> Tuple2<List<T>, List<Throwable>> partition(Iterable<Try<T>> values) {
         var oks = new ArrayList<T>();
         var errs = new ArrayList<Throwable>();
 
@@ -49,14 +49,14 @@ public final class Tries {
         return Tuple.of(List.copyOf(oks), List.copyOf(errs));
     }
 
-    static <T> List<T> anyValues(Iterable<Try<T>> values) {
+    public static <T> List<T> anyValues(Iterable<Try<T>> values) {
         return StreamSupport.stream(values.spliterator(), false)
                 .filter(Try::isSuccess)
                 .map(Try::get)
                 .toList();
     }
 
-    static <T> Try<List<T>> allValues(Iterable<Try<T>> values) {
+    public static <T> Try<List<T>> allValues(Iterable<Try<T>> values) {
         var partition = partition(values);
         var ok = partition._1;
         var errs = partition._2;
@@ -64,7 +64,7 @@ public final class Tries {
         return errs.isEmpty() ? Try.success(ok) : Try.failure(errs.get(0));
     }
 
-    static <T, T1> Try<T1> foldResult(Iterable<T> values, Try<T1> initial, Function2<T1, T, Try<T1>> operation) {
+    public static <T, T1> Try<T1> foldResult(Iterable<T> values, Try<T1> initial, Function2<T1, T, Try<T1>> operation) {
         var accumulator = initial;
         for (T el : values) {
             accumulator = accumulator.flatMap(value -> operation.apply(value, el));
@@ -72,7 +72,7 @@ public final class Tries {
         return accumulator;
     }
 
-    static <T, T1> Try<List<T1>> mapAllValues(Iterable<T> values, Function<T, Try<T1>> f) {
+    public static <T, T1> Try<List<T1>> mapAllValues(Iterable<T> values, Function<T, Try<T1>> f) {
         var acc = new ArrayList<T1>();
 
         for (T value : values) {
