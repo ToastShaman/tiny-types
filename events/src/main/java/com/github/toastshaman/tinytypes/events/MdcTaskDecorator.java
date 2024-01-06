0ac8d75ce13e5c47ab44aebccca2302f9d0a0096
Kevin Denver <1027637+ToastShaman@users.noreply.github.com>
2024-01-06 22:28:35 +0000

    feat: add MDC task decorator

---
+++ events/src/main/java/com/github/toastshaman/tinytypes/events/MdcTaskDecorator.java
@@ -0,0 +1,31 @@
+package com.github.toastshaman.tinytypes.events;
+
+import org.slf4j.MDC;
+
+import java.util.function.UnaryOperator;
+
+public final class MdcTaskDecorator implements UnaryOperator<Runnable> {
+
+    @Override
+    public Runnable apply(Runnable runnable) {
+        return decorate(runnable);
+    }
+
+    public static Runnable decorate(Runnable command) {
+        var copy = MDC.getCopyOfContextMap();
+
+        return () -> {
+            if (copy == null || copy.isEmpty()) {
+                command.run();
+                return;
+            }
+
+            try {
+                MDC.setContextMap(copy);
+                command.run();
+            } finally {
+                MDC.clear();
+            }
+        };
+    }
+}
