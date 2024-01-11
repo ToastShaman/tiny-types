89aab0060672fe9d225bc0be20da7949fd433f14
Kevin Denver <1027637+ToastShaman@users.noreply.github.com>
2024-01-11 16:50:25 +0000

    refactor: JacksonEventLogger

--- events-format-jackson/src/main/java/com/github/toastshaman/tinytypes/events/format/jackson/JacksonEventLogger.java
+++ events-format-jackson/src/main/java/com/github/toastshaman/tinytypes/events/format/jackson/JacksonEventLogger.java
@@ -2,6 +2,14 @@ package com.github.toastshaman.tinytypes.events.format.jackson;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 import static com.fasterxml.jackson.core.JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN;
+import static com.fasterxml.jackson.databind.DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY;
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES;
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES;
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS;
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+import static com.fasterxml.jackson.databind.DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS;
+import static com.fasterxml.jackson.databind.DeserializationFeature.USE_BIG_INTEGER_FOR_INTS;
+import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
 import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -10,55 +18,60 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.github.toastshaman.tinytypes.events.Event;
 import com.github.toastshaman.tinytypes.events.Events;
+
 import java.util.Objects;
 import java.util.function.Consumer;
 
 public final class JacksonEventLogger implements Events, Consumer<String> {
 
-    private final Consumer<String> writer;
+    private final Consumer<String> consumer;
 
-    private final ObjectWriter json;
+    private final ObjectWriter objectWriter;
+
+    private static final JsonMapper jsonMapper = JsonMapper.builder()
+            .deactivateDefaultTyping()
+            .serializationInclusion(NON_NULL)
+            .enable(FAIL_ON_NULL_FOR_PRIMITIVES)
+            .enable(FAIL_ON_NUMBERS_FOR_ENUMS)
+            .enable(USE_BIG_DECIMAL_FOR_FLOATS)
+            .enable(USE_BIG_INTEGER_FOR_INTS)
+            .enable(WRITE_BIGDECIMAL_AS_PLAIN)
+            .disable(INDENT_OUTPUT)
+            .disable(WRITE_DATES_AS_TIMESTAMPS)
+            .disable(FAIL_ON_UNKNOWN_PROPERTIES)
+            .disable(FAIL_ON_IGNORED_PROPERTIES)
+            .disable(ACCEPT_SINGLE_VALUE_AS_ARRAY)
+            .addModule(new EventsModule())
+            .build();
 
     public JacksonEventLogger() {
-        this(JsonMapper.builder()
-                .addModule(new EventsModule())
-                .enable(WRITE_BIGDECIMAL_AS_PLAIN)
-                .disable(WRITE_DATES_AS_TIMESTAMPS)
-                .serializationInclusion(NON_NULL)
-                .build());
+        this(jsonMapper);
     }
 
-    public JacksonEventLogger(Consumer<String> writer) {
-        this(
-                writer,
-                JsonMapper.builder()
-                        .addModule(new EventsModule())
-                        .enable(WRITE_BIGDECIMAL_AS_PLAIN)
-                        .disable(WRITE_DATES_AS_TIMESTAMPS)
-                        .serializationInclusion(NON_NULL)
-                        .build());
+    public JacksonEventLogger(Consumer<String> consumer) {
+        this(jsonMapper, consumer);
     }
 
     public JacksonEventLogger(ObjectMapper mapper) {
-        this(System.out::println, mapper);
+        this(mapper, System.out::println);
     }
 
-    public JacksonEventLogger(Consumer<String> writer, ObjectMapper mapper) {
-        this.writer = Objects.requireNonNull(writer, "writer must not be null");
-        this.json = Objects.requireNonNull(mapper, "mapper must not be null").writer();
+    public JacksonEventLogger(ObjectMapper mapper, Consumer<String> consumer) {
+        this.consumer = Objects.requireNonNull(consumer);
+        this.objectWriter = Objects.requireNonNull(mapper).writer();
     }
 
     @Override
     public void record(Event event) {
         try {
-            accept(json.writeValueAsString(event));
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+            accept(objectWriter.writeValueAsString(event));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to write event: %s".formatted(e.getMessage()), e);
         }
     }
 
     @Override
     public void accept(String text) {
-        writer.accept(text);
+        consumer.accept(text);
     }
 }
