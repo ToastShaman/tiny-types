91ed89629afbe1884bacec4be4f7341a32be0448
Kevin Denver <1027637+ToastShaman@users.noreply.github.com>
2025-10-25 15:32:53 +0100

    refactor: simplify

--- aws-sqs-connector/src/test/java/com/github/toastshaman/tinytypes/aws/sqs/SqsHeaderTest.java
+++ aws-sqs-connector/src/test/java/com/github/toastshaman/tinytypes/aws/sqs/SqsHeaderTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.services.sqs.model.Message;
 import software.amazon.awssdk.services.sqs.model.MessageAttributeValue;
@@ -14,30 +15,53 @@ class SqsHeaderTest {
 
     public static final SqsHeader<String> TEST_HEADER = SqsHeader.text("x-my-test-header");
 
-    @Test
-    void can_extract_header() {
-        var message = Message.builder()
-                .messageAttributes(Map.of(
-                        "x-my-test-header",
-                        MessageAttributeValue.builder()
-                                .dataType("String")
-                                .stringValue("header-value")
-                                .build()))
-                .build();
+    @Nested
+    class Text {
+        @Test
+        void can_extract_header() {
+            var message = Message.builder()
+                    .messageAttributes(Map.of(
+                            "x-my-test-header",
+                            MessageAttributeValue.builder()
+                                    .dataType("String")
+                                    .stringValue("header-value")
+                                    .build()))
+                    .build();
 
-        var extractedValue = TEST_HEADER.get(message);
+            var extractedValue = TEST_HEADER.get(message);
 
-        assertThat(extractedValue).isEqualTo("header-value");
+            assertThat(extractedValue).isEqualTo("header-value");
+        }
+
+        @Test
+        void can_set_header() {
+            var message = Message.builder()
+                    .messageAttributes(Map.of("x-my-test-header", TEST_HEADER.reverseGet("header-value")))
+                    .build();
+
+            var extractedValue = TEST_HEADER.get(message);
+
+            assertThat(extractedValue).isEqualTo("header-value");
+        }
     }
 
-    @Test
-    void can_set_header() {
-        var message = Message.builder()
-                .messageAttributes(Map.of("x-my-test-header", TEST_HEADER.reverseGet("header-value")))
-                .build();
+    @Nested
+    class Timestamp {
+        @Test
+        void can_extract_header() {
+            var message = Message.builder()
+                    .messageAttributes(Map.of(
+                            "x-my-test-header",
+                            MessageAttributeValue.builder()
+                                    .dataType("String")
+                                    .stringValue("2024-06-01T12:34:56Z")
+                                    .build()))
+                    .build();
 
-        var extractedValue = TEST_HEADER.get(message);
+            var timestampHeader = SqsHeader.timestamp("x-my-test-header");
+            var extractedValue = timestampHeader.get(message);
 
-        assertThat(extractedValue).isEqualTo("header-value");
+            assertThat(extractedValue).isEqualTo("2024-06-01T12:34:56Z");
+        }
     }
 }
