915b60f3ca794f91ee79cb7f984154f034600784
Kevin Denver <1027637+ToastShaman@users.noreply.github.com>
2025-10-28 12:49:21 +0000

    feat: add ScheduledSqsMessageListener

--- aws-sqs-connector/src/main/java/com/github/toastshaman/tinytypes/aws/sqs/ScheduledSqsMessageListener.java
+++ aws-sqs-connector/src/main/java/com/github/toastshaman/tinytypes/aws/sqs/ScheduledSqsMessageListener.java
@@ -10,7 +10,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public final class ScheduledSqsMessageListener implements AutoCloseable{
+public final class ScheduledSqsMessageListener implements AutoCloseable {
 
     private static final Logger log = LoggerFactory.getLogger(ScheduledSqsMessageListener.class);
 
