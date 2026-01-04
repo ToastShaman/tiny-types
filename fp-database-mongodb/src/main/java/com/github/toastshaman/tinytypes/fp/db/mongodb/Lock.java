2ec627dd3692047e764199297a16b05855a8c519
Kevin Denver <1027637+ToastShaman@users.noreply.github.com>
2026-01-04 16:50:55 +0000

    feat: add lock

--- fp-database-mongodb/src/main/java/com/github/toastshaman/tinytypes/fp/db/mongodb/Lock.java
+++ fp-database-mongodb/src/main/java/com/github/toastshaman/tinytypes/fp/db/mongodb/Lock.java
@@ -2,19 +2,25 @@ package com.github.toastshaman.tinytypes.fp.db.mongodb;
 
 import io.vavr.Function0;
 import io.vavr.Function1;
+import java.util.Objects;
 import java.util.Optional;
 import net.javacrumbs.shedlock.core.LockConfiguration;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.core.SimpleLock;
 
-public record Lock(LockProvider p, LockConfiguration config) {
+public record Lock(LockProvider provider, LockConfiguration config) {
+
+    public Lock {
+        Objects.requireNonNull(provider, "LockProvider must not be null");
+        Objects.requireNonNull(config, "LockConfiguration must not be null");
+    }
 
     public <R> Optional<R> executeMaybe(Function0<R> fn) {
         return executeMaybe(_ -> fn.apply());
     }
 
     public <R> Optional<R> executeMaybe(Function1<SimpleLock, R> fn) {
-        SimpleLock lock = p.lock(config).orElse(null);
+        SimpleLock lock = provider.lock(config).orElse(null);
 
         if (lock == null) {
             return Optional.empty();
